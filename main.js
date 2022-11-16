04f2773fc4da430289c8443f30a7e38e1a464b6b
Clinton <clintonphillips464@gmail.com>
2022-11-16 16:20:17 +0100

    finished code

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ window.onload = function () {
     })
 }
 
+var swiper = new Swiper(".mySwiper", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
